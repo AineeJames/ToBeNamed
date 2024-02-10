0713810a5e356f5e523b1187f41757f688935a86
AineeJames <olsenaiden33@gmail.com>
2024-02-10 14:05:40 -0700

    smoother moves

--- scenes/character/mainChar.gd
+++ scenes/character/mainChar.gd
@@ -1,34 +1,22 @@
 extends CharacterBody2D
 
-
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+@export var movement_speed: float = 150.0
+@export var acceleration: float = 800.0
+@export var deceleration: float = 1200.0
+@export var max_velocity: float = 200.0
 
 func _physics_process(delta):
-	# Add the gravity.
-	#if not is_on_floor():
-	#	velocity.y += gravity * delta
-
-	# Handle jump.
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-	#	velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Get the input direction and handle the movement/acceleration/deceleration.
 	var direction = Input.get_axis("move_left", "move_right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = move_toward(velocity.x, direction * max_velocity, acceleration * delta)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, deceleration * delta)
 
 	var ydirection = Input.get_axis("move_up", "move_down")
 	if ydirection:
-		velocity.y = ydirection * SPEED
+		velocity.y = move_toward(velocity.y, ydirection * max_velocity, acceleration * delta)
 	else:
-		velocity.y = move_toward(velocity.y, 0, SPEED)
+		velocity.y = move_toward(velocity.y, 0, deceleration * delta)    
 
 	move_and_slide()
