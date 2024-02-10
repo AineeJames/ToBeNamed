9e17a14e1c60fbfa7527f8e287882102673c27cf
James Wilcock <jameswilcock8@gmail.com>
2024-02-10 14:15:03 -0800

    added timer to bullet to delete after 10 seconds

--- scenes/guns/bullet.gd
+++ scenes/guns/bullet.gd
@@ -1,4 +1,22 @@
 extends CharacterBody2D
 
+func _ready():
+	# Create a new Timer node.
+	var timer = Timer.new()
+	# Set the timer to trigger once after 20 seconds.
+	timer.wait_time = 10
+	timer.one_shot = true
+	# Add the timer as a child of the current node.
+	add_child(timer)
+	# Connect the timer's "timeout" signal to a custom function.
+	timer.connect("timeout", _on_timer_timeout)
+	# Start the timer.
+	timer.start()
+
+func _on_timer_timeout():
+	# Function to be called when the timer reaches 0.
+	# This will delete the current node.
+	queue_free()
+
 func _physics_process(delta):
 	move_and_collide(velocity)
