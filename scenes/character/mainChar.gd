26f7825ede8cf69aaf7c6a04b755136abc4baafa
AineeJames <olsenaiden33@gmail.com>
2024-02-12 20:22:59 -0700

    added controller capability

--- scenes/character/mainChar.gd
+++ scenes/character/mainChar.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
-var gun_distance_from_player = 50
+@export var gun_distance_from_player = 35
+var angle_of_gun = 0
 
 @export var GunSprite: Sprite2D
 
@@ -24,12 +25,22 @@ func _physics_process(delta):
 		velocity.y = move_toward(velocity.y, ydirection * max_velocity, acceleration * delta)
 	else:
 		velocity.y = move_toward(velocity.y, 0, deceleration * delta)
+		
+	if Input.is_action_just_pressed("dash"):
+		velocity.x = move_toward(velocity.x, velocity.x * 5, acceleration * delta * 25)
+		velocity.y = move_toward(velocity.y, velocity.y * 5, acceleration * delta * 25)
 
 	move_and_slide()
 	
-	var mouse_pos = get_global_mouse_position()
-	var angle_to_mouse = (mouse_pos - global_position).angle()
-	
+	var deadzone = 0.1
+	if Input.get_connected_joypads().size() > 0:
+		var x = Input.get_joy_axis(0, JOY_AXIS_RIGHT_X)
+		var y = Input.get_joy_axis(0, JOY_AXIS_RIGHT_Y)
+		if abs(x) > deadzone || abs(y) > deadzone:
+			angle_of_gun = Vector2(x, y).angle()
+	else:
+		var mouse_pos = get_global_mouse_position()
+		angle_of_gun = (mouse_pos - global_position).angle()
 
-	Gun.global_position = global_position + Vector2(cos(angle_to_mouse), sin(angle_to_mouse)) * gun_distance_from_player
+	Gun.global_position = global_position + Vector2(cos(angle_of_gun), sin(angle_of_gun)) * gun_distance_from_player
 	Gun.look_at(global_position)
