4b81dcf5d3f8ec4497cec0f81c2a7edda1ad3817
AineeJames <olsenaiden33@gmail.com>
2024-02-13 23:02:42 -0700

    added assult rifle

--- scenes/weapons/guns/gun.gd
+++ scenes/weapons/guns/gun.gd
@@ -10,11 +10,14 @@ extends Node2D
 @onready var ReloadingBar = $Control/CenterContainer/ReloadingProgressBar
 @onready var ClipBar = $Control/CenterContainer/ClipBar
 @onready var BulletCounter = $Control/BulletsCountLabel
+@onready var FireRateTimer = $FireRateTimer
 @onready var Bullet = load("res://scenes/weapons/guns/bullet/bullet.tscn")
 
 var velocity = 0
 var prev_position: Vector2
 var bullets_remaining: int
+var can_shoot = true
+var fire_held = false
 
 func _ready():
 	GunSprite.texture = selected_gun.gun_texture
@@ -32,23 +35,37 @@ func _physics_process(delta):
 	ReloadingBar.value = ReloadTimer.time_left
 	ClipBar.value = bullets_remaining
 	BulletCounter.text = str(bullets_remaining)
+	if fire_held and can_shoot and bullets_remaining > 0:
+		if selected_gun.automatic:
+			can_shoot = false
+			fire_bullet()
+			FireRateTimer.start(1. / selected_gun.fire_rate)
 
 func _input(event):
 	if is_ai_gun:
 		return
+		
+	if event.is_action_pressed("fire"):
+		fire_held = true
+	if event.is_action_released("fire"):
+		fire_held = false
 		
 	if bullets_remaining > 0 and event.is_action_pressed("reload"):
 		ReloadingBar.visible = true
 		ClipBar.visible = false
 		ReloadTimer.start(selected_gun.reload_time)
 	
-	if not selected_gun.automatic and event.is_action_pressed("fire") and bullets_remaining > 0:
+	if not selected_gun.automatic and event.is_action_pressed("fire") and bullets_remaining > 0 and can_shoot:
 		fire_bullet()
+		
 		
 func _on_reload_timer_timeout():
 	bullets_remaining = selected_gun.clip_size
 	ReloadingBar.visible = false
 	ClipBar.visible = true
+
+func _on_fire_rate_timer_timeout():
+	can_shoot = true
 
 func fire_bullet():
 	
@@ -65,15 +82,14 @@ func fire_bullet():
 	instance.velocity = velocity * movement_influence + Vector2(selected_gun.bullet_speed*cos(rotation + PI), selected_gun.bullet_speed*sin(rotation + PI))
 	instance.crit = randi_range(0, 100) < int(selected_gun.crit_chance * 100)
 	instance.damage = selected_gun.bullet_damage
-	instance.bullet_speed = selected_gun.bullet_damage
+	instance.bullet_speed = selected_gun.bullet_speed
 	get_tree().current_scene.add_child(instance)
 	if !is_ai_gun and instance.crit:
 		GlobalEventBus.player_did_crit.emit()
-		
+	
 	bullets_remaining -= 1
 	if bullets_remaining == 0:
 		ReloadingBar.visible = true
 		ClipBar.visible = false
 		ReloadTimer.start(selected_gun.reload_time)
-		
-		
+
