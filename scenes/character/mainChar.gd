3ea8d2a4af62cc9a2db59800339a6aaae81f5229
James Wilcock <jameswilcock8@gmail.com>
2024-02-15 19:34:07 -0800

    gun not flashing in the middle of char on creation

--- scenes/character/mainChar.gd
+++ scenes/character/mainChar.gd
@@ -26,14 +26,28 @@ var Gun
 @onready var GunScene = load("res://scenes/weapons/guns/gun.tscn")
 
 var total_damage = 0
+var deadzone = 0.1
 signal take_damage(amount, bump_direction)
 
+func position_gun():
+	if Input.get_connected_joypads().size() > 0:
+		var x = Input.get_joy_axis(0, JOY_AXIS_RIGHT_X)
+		var y = Input.get_joy_axis(0, JOY_AXIS_RIGHT_Y)
+		if abs(x) > deadzone || abs(y) > deadzone:
+			angle_of_gun = Vector2(x, y).angle()
+	else:
+		var mouse_pos = get_global_mouse_position()
+		angle_of_gun = (mouse_pos - global_position).angle()
+	Gun.global_position = global_position + Vector2(cos(angle_of_gun), sin(angle_of_gun)) * gun_distance_from_player
+	Gun.look_at(global_position)
+	
 func create_gun():
 	if Gun:
 		Gun.queue_free()
 	Gun = GunScene.instantiate()
 	Gun.selected_gun = guns[gun_index]
 	%SelectedWeaponName.text = "Weapon: " + Gun.selected_gun.gun_name
+	position_gun()
 	add_child(Gun)
 
 func _input(event):
@@ -89,18 +103,8 @@ func _physics_process(delta):
 
 	move_and_slide()
 	
-	var deadzone = 0.1
-	if Input.get_connected_joypads().size() > 0:
-		var x = Input.get_joy_axis(0, JOY_AXIS_RIGHT_X)
-		var y = Input.get_joy_axis(0, JOY_AXIS_RIGHT_Y)
-		if abs(x) > deadzone || abs(y) > deadzone:
-			angle_of_gun = Vector2(x, y).angle()
-	else:
-		var mouse_pos = get_global_mouse_position()
-		angle_of_gun = (mouse_pos - global_position).angle()
 
-	Gun.global_position = global_position + Vector2(cos(angle_of_gun), sin(angle_of_gun)) * gun_distance_from_player
-	Gun.look_at(global_position)
+	position_gun()
 
 func took_damage(amount, bump_vector):
 	print("took ", amount , "damage")
